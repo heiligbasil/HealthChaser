1465550febf7de5640f6c6c98113581b3a7a6a3d
heiligbasil <basilief@yahoo.com>
2019-05-22 12:38:47 -0700

    Added a TimePicker DialogFragment to a button onClick

--- app/src/main/java/com/lambdaschool/healthchaser/GenericMasterActivity.java
+++ app/src/main/java/com/lambdaschool/healthchaser/GenericMasterActivity.java
@@ -6,6 +6,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -144,5 +145,10 @@ public class GenericMasterActivity extends AppCompatActivity {
                 genericMasterActivityAdapter.notifyDataSetChanged();
             }
         });
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
     }
 }
